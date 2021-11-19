7f0ae5c46088a68d5a62f4f040d1db511d9ee380
ClemLya <clementlyara72@gmail.com>
2021-11-19 22:36:09 +0100

    V1.0.2

--- gestion/view.php
+++ gestion/view.php
@@ -1,3 +1,12 @@
 <?php
+  // On ajoute le fichier css
+  function wpdocs_register_plugin_styles(){
+    wp_register_style('plan', plugins_url('plan/style/style.css'));
+    wp_enqueue_style('plan');
+  }
+  // Register style sheet.
+  add_action( 'wp_enqueue_scripts', 'wpdocs_register_plugin_styles' );
+
+
   echo "ee";
  ?>
