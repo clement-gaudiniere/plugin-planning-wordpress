0e41f3ba9dd7703fb639965ef083624c8a301ddb
ClemLya <clementlyara72@gmail.com>
2021-11-20 19:35:00 +0100

    V1.0.3

--- gestion/view.php
+++ gestion/view.php
@@ -1,12 +1,95 @@
-<?php
-  // On ajoute le fichier css
-  function wpdocs_register_plugin_styles(){
-    wp_register_style('plan', plugins_url('plan/style/style.css'));
-    wp_enqueue_style('plan');
-  }
-  // Register style sheet.
-  add_action( 'wp_enqueue_scripts', 'wpdocs_register_plugin_styles' );
+<section>
+  <div class="part">
+    <h3>Crénaux adultes loisirs</h3>
+    <table>
+      <tr>
+        <th>Mercredi</th>
+        <th>Jeudi</th>
+        <th>Vendredi</th>
+        <th>Dimanche</th>
+      </tr>
+      <tr>
+        <td>
+          <input type="time" id="crenauxAdultesLoisirsMercredi" name="" value="20:30" required>
+          <span>-</span>
+          <input type="time" id="crenauxAdultesLoisirsMercredi" name="" value="22:00" required>
+        </td>
+        <td>
+          <input type="time" id="crenauxAdultesLoisirsJeudi" name="" value="20:30" required>
+          <span>-</span>
+          <input type="time" id="crenauxAdultesLoisirsJeudi" name="" value="22:00" required>
+        </td>
+        <td>
+          <input type="time" id="crenauxAdultesLoisirsVendredi" name="" value="20:30" required>
+          <span>-</span>
+          <input type="time" id="crenauxAdultesLoisirsVendredi" name="" value="22:00" required>
+        </td>
+        <td>
+          <input type="time" id="crenauxAdultesLoisirsDimanche" name="" value="09:00" required>
+          <span>-</span>
+          <input type="time" id="crenauxAdultesLoisirsDimanche" name="" value="12:00" required>
+        </td>
+      </tr>
+      <tr>
+        <td class="comment"><textarea placeholder="Commentaire..."></textarea></td>
+        <td class="comment"><textarea placeholder="Commentaire..."></textarea></td>
+        <td class="comment"><textarea placeholder="Commentaire..."></textarea></td>
+        <td class="comment"><textarea placeholder="Commentaire..."></textarea></td>
+      </tr>
+    </table>
+    <div class="toolbar">
+      <button type="button" name="button">Ajouter une exception</button>
+      <button type="button" name="button">Enregistrer</button>
+    </div>
+  </div>
+  <div class="part">
+    <h3>Crénaux habituels pour les adultes confirmés</h3>
+    <table>
+      <tr>
+        <th>Lundi</th>
+        <th>Mardi</th>
+        <th>Mercredi</th>
+        <th>Jeudi</th>
+        <th>Vendredi</th>
+        <th>Samedi</th>
+        <th>Dimanche</th>
+      </tr>
+      <tr>
+        <td>19H30 - 21H</td>
+        <td>Maria Anders</td>
+        <td>Germany</td>
+        <td>Alfreds Futterkiste</td>
+        <td>Maria Anders</td>
+        <td>Germany</td>
+        <td>Germany</td>
+      </tr>
+      <tr>
+        <td>Alfreds Futterkiste</td>
+        <td>Maria Anders</td>
+        <td>Germany</td>
+        <td>Alfreds Futterkiste</td>
+        <td>Maria Anders</td>
+        <td>Germany</td>
+        <td>Germany</td>
+      </tr>
+    </table>
+  </div>
 
+  <div class="part">
+    <div id="ajouterPlanning">
+      <svg xmlns="http://www.w3.org/2000/svg" width="25px" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" viewBox="0 0 489.8 489.8" style="enable-background:new 0 0 489.8 489.8;" xml:space="preserve">
+      	<g>
+      		<path d="M438.2,0H51.6C23.1,0,0,23.2,0,51.6v386.6c0,28.5,23.2,51.6,51.6,51.6h386.6c28.5,0,51.6-23.2,51.6-51.6V51.6    C489.8,23.2,466.6,0,438.2,0z M465.3,438.2c0,14.9-12.2,27.1-27.1,27.1H51.6c-14.9,0-27.1-12.2-27.1-27.1V51.6    c0-14.9,12.2-27.1,27.1-27.1h386.6c14.9,0,27.1,12.2,27.1,27.1V438.2z"/>
+      		<path d="M337.4,232.7h-80.3v-80.3c0-6.8-5.5-12.3-12.3-12.3s-12.3,5.5-12.3,12.3v80.3h-80.3c-6.8,0-12.3,5.5-12.3,12.2    c0,6.8,5.5,12.3,12.3,12.3h80.3v80.3c0,6.8,5.5,12.3,12.3,12.3s12.3-5.5,12.3-12.3v-80.3h80.3c6.8,0,12.3-5.5,12.3-12.3    C349.7,238.1,344.2,232.7,337.4,232.7z"/>
+      	</g>
+      </svg>
+      Ajouter un planning horaires
+    </div>
+  </div>
 
-  echo "ee";
- ?>
+<script type="text/javascript">
+jQuery(function( $ ) {
+  // $('body').css('background','red');
+});
+</script>
+</section>
